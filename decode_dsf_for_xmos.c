ff50983d6c8c59d60832aa3ab8c4ebad8c26bbe8
BayernMuller <cjjunk29@gmail.com>
2024-07-03 14:45:16 +0900

    refactor: optimize dsfxmos function

--- decode_dsf_for_xmos.c
+++ decode_dsf_for_xmos.c
@@ -5,35 +5,32 @@ unsigned char revbits(unsigned char x) {
     return x;
 }
 
-void dsfxmos(int size, unsigned char *indata, unsigned char *outdata, int lsbfirst) {
+void dsfxmos(int size, unsigned char *indata, unsigned char *outdata, int lsbfirst)
+{
     int j = 0;
-    if (lsbfirst == 1) {
+    if (lsbfirst == 1)
+    {
         for (int i = 0; i < size; i += 8) {
             outdata[i + 0x00] = revbits(indata[j + 0x00]);
             outdata[i + 0x01] = revbits(indata[j + 0x01]);
             outdata[i + 0x02] = revbits(indata[j + 0x02]);
             outdata[i + 0x03] = revbits(indata[j + 0x03]);
-            j += 4;
-        }
-        j = 0;
-        for (int i = 0; i < size; i += 8) {
             outdata[i + 0x04] = revbits(indata[j + 4096 + 0x00]);
             outdata[i + 0x05] = revbits(indata[j + 4096 + 0x01]);
             outdata[i + 0x06] = revbits(indata[j + 4096 + 0x02]);
             outdata[i + 0x07] = revbits(indata[j + 4096 + 0x03]);
             j += 4;
         }
-    } else {
+
+    }
+    else 
+    {
         j = 0;
         for (int i = 0; i < size; i += 8) {
             outdata[i + 0x00] = indata[j + 0x00];
             outdata[i + 0x01] = indata[j + 0x01];
             outdata[i + 0x02] = indata[j + 0x02];
             outdata[i + 0x03] = indata[j + 0x03];
-            j += 4;
-        }
-        j = 0;
-        for (int i = 0; i < size; i += 8) {
             outdata[i + 0x04] = indata[j + 4096 + 0x00];
             outdata[i + 0x05] = indata[j + 4096 + 0x01];
             outdata[i + 0x06] = indata[j + 4096 + 0x02];
